5697279d81fa114eb75084f77d861dad0d1191f6
illnorbert <illes.norbert17@gmail.com>
2021-03-30 13:40:18 +0200

    eseményfigyelőgomb

--- app/src/main/java/com/example/elsoproject/MainActivity.java
+++ app/src/main/java/com/example/elsoproject/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.elsoproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -13,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button gomb1 = findViewById(R.id.gomb);
+        EditText info = findViewById(R.id.edittext);
+
+        gomb1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Intent i = new Intent(MainActivity.this, NewActivity.class);
+                System.out.println("Hello World");
+            }
+        });
     }
 }
