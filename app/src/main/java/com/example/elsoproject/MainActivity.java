b2c6f7c7d887459c32ca81c2cb1535b18cf01ea1
MoriczJanos113C <71004440+MoriczJanos113C@users.noreply.github.com>
2021-04-09 12:07:42 +0200

    MainActivity kísz

--- app/src/main/java/com/example/elsoproject/MainActivity.java
+++ app/src/main/java/com/example/elsoproject/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.elsoproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    EditText karakter = findViewById(R.id.karakter);
+    EditText Skin = findViewById(R.id.Skin);
+
+    RadioGroup gombok = findViewById(R.id.gombok);
+
+    Button kuldes = findViewById(R.id.kuldes);
+
+    kuldes.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(MainActivity.this, NewActivity.class);
+            i.putExtra("uzenet", karakter.getText().toString());
+            i.putExtra("uzenet", Skin.getText().toString());
+
+            i.putExtra("key","value");
+
+            startActivity(i);
+
+        }
+    });
 
     }
 }
